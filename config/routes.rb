c7153b98186bf19594055c1068d3da4256d77c26
xiaoyingquan <xiaoyingquan1@gmail.com>
2017-08-20 16:17:46 +0800

    add authController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
+
+      post "/signup" => "auth#signup"
+      post "/login" => "auth#login"
+      post "/logout" => "auth#logout"
+
       get "/reservations" => "reservations#index", :as => :reservations
       get "/trains"  => "trains#index", :as => :trains
       get "/trains/:train_number" => "trains#show", :as => :train
