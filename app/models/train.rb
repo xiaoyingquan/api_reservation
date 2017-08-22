47ab338d5a493394e411b0b7124e1cffa10a41d4
xiaoyingquan <xiaoyingquan1@gmail.com>
2017-08-23 07:06:52 +0800

    add train logo image

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,6 +2,8 @@ class Train < ApplicationRecord
   validates_presence_of :number
   has_many :reservations
 
+  mount_uploader :train_logo, TrainLogoUploader
+
   # 产生所有位置从 1A～6C
   # ["1A", "1B", "1C", "2A", "2B", "2C", "3A", "3B", "3C",
   #  "4A", "4B", "4C", "5A", "5B", "5C", "6A", "6B", "6C"]
@@ -17,5 +19,5 @@ class Train < ApplicationRecord
     # 所有 SEATS 扣掉已经订位的资料
     return SEATS - self.reservations.pluck(:seat_number)
   end
-  
+
 end
